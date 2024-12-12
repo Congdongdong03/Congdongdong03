96b4892c354cd016408f9d3b8565f89251e8fd8c
Wesley <congdongdong03@gmail.com>
2024-12-12 17:47:20 +1100

    header 编写

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useDidShow, useDidHide } from "@tarojs/taro";
+import "@nutui/nutui-react-taro/dist/style.css";
 // 全局样式
 import "./app.scss";
 
--- src/pages/components/manu/Businessheader/index.jsx
+++ src/pages/components/manu/Businessheader/index.jsx
@@ -1,16 +1,28 @@
 import { View } from "@tarojs/components";
+
+// 图片引入
+import userPicture from "/src/pages/picture/user_picture.jpg";
 import "./index.scss";
+// 然后在组件中使用
 
 const BusinessHeader = () => {
   return (
     <View className="business-header">
       <View className="header-area">
-        <View className="business-info">商家名称</View>
-        <View className="business-details">地址：某某街道</View>
+        {/* 图片 */}
+        <View className="header-userPicture">
+          <image src={userPicture} mode="aspectFit" />
+        </View>
+        <View className="business-info">
+          <View className="business-details">商家名称</View>
+          <View className="business-details">地址：某某街道</View>
+        </View>
       </View>
       <View className="notice-area">
-        <View className="notice-title">通知：</View>
-        <View className="notice-content">今天有优惠活动，满100减10！</View>
+        <View className="notice-info">
+          <View className="notice-title">通知：</View>
+          <View className="notice-content">今天有优惠活动，满100减10！</View>
+        </View>
       </View>
     </View>
   );
