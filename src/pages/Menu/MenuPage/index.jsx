9320328ac2f64faa61bb96f5fee1eff39474a166
Wesley <congdongdong03@gmail.com>
2024-12-19 11:58:40 +1100

    代码合并迁移

--- src/pages/Menu/MenuPage/index.jsx
+++ src/pages/Menu/MenuPage/index.jsx
@@ -2,16 +2,14 @@ import React, { useEffect, useState } from "react";
 import { View } from "@tarojs/components";
 import BusinessHeader from "../BusinessHeader";
 import CategoryMenu from "../CategoryMenu";
-import ProductList from "../ProductList";
 import Taro from "@tarojs/taro";
 import "./index.scss";
-import { fetchCategories } from "../../../services/api"; // 引入封装好的接口方法
+import { fetchCategories } from "../../../services/api";
+
 const MenuPage = () => {
-  // 将 useState 移到组件内部，这是正确的位置
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    // 使用封装的接口方法
     fetchCategories()
       .then((data) => {
         console.log("接口调用成功：", data);
@@ -21,6 +19,7 @@ const MenuPage = () => {
         console.error("接口调用失败：", err);
       });
   }, []);
+
   try {
     return (
       <View className="menu-page" style={{ height: "100vh" }}>
@@ -42,19 +41,8 @@ const MenuPage = () => {
             overflowY: "auto",
           }}
         >
-          <View
-            className="category-menu"
-            style={{
-              overflowY: "auto",
-              backgroundColor: "rgb(255, 182, 193)",
-              borderTopRightRadius: "10px",
-            }}
-          >
-            <CategoryMenu categories={categories} />
-          </View>
-          <View className="product-list" style={{ overflowY: "auto" }}>
-            <ProductList categories={categories} />
-          </View>
+          {/* 移除了单独的category-menu和product-list容器，因为它们现在在CategoryMenu组件中统一管理 */}
+          <CategoryMenu categories={categories} />
         </View>
       </View>
     );
