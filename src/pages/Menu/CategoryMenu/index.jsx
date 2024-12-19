c6ad9d900a6fa7d3b4c25a645e3e13c2ff047cc6
Wesley <congdongdong03@gmail.com>
2024-12-19 11:58:40 +1100

    代码合并迁移

--- src/pages/Menu/CategoryMenu/index.jsx
+++ src/pages/Menu/CategoryMenu/index.jsx
@@ -1,15 +1,46 @@
 import { View } from "@tarojs/components";
-import { useState } from "react";
+import React, { useState } from "react";
+import { InputNumber, Badge } from "@nutui/nutui-react-taro";
+import ProductImage from "../../../components/index";
+import plusIcon from "../../../assets/icons/plus.png";
+import shoppingCar from "../../../assets/icons/shoppingcar.png";
 import "./index.scss";
 
 const CategoryMenu = ({ categories }) => {
-  // 定义一个状态来存储当前选中的菜单项
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [showInputNumbers, setShowInputNumbers] = useState({});
+  const [inputValues, setInputValues] = useState({});
 
   const handleCategoryClick = (id) => {
-    console.log("1");
-    console.log("Selected ID:", id); // 打印被点击的菜单项 ID
-    setSelectedCategory(id); // 设置当前选中的菜单项 ID
+    setSelectedCategory(id);
+  };
+
+  const toggleInputNumber = (dishId) => {
+    setShowInputNumbers((prevState) => ({
+      ...prevState,
+      [dishId]: !prevState[dishId],
+    }));
+    setInputValues((prevState) => ({
+      ...prevState,
+      [dishId]: 1,
+    }));
+  };
+
+  const handleValueChange = (value, dishId) => {
+    setInputValues((prevState) => ({
+      ...prevState,
+      [dishId]: value,
+    }));
+  };
+
+  const handleMinusClick = (dishId) => {
+    const currentValue = inputValues[dishId];
+    if (currentValue === 1) {
+      setShowInputNumbers((prevState) => ({
+        ...prevState,
+        [dishId]: false,
+      }));
+    }
   };
 
   return (
@@ -22,14 +53,73 @@ const CategoryMenu = ({ categories }) => {
             className={`menu-item ${
               selectedCategory === item.id ? "active" : ""
             }`}
-            onClick={() => {
-              console.log("Clicked ID:", item.id); // 直接打印点击的 ID
-              handleCategoryClick(item.id); // 调用事件处理器
-            }}
+            onClick={() => handleCategoryClick(item.id)}
           >
             {item.name}
           </View>
         ))}
+      </View>
+
+      <View className="product-list">
+        {categories.map((category) => (
+          <View key={category.id}>
+            <View className="category-title">{category.name}</View>
+            {category.dishes?.map((dish) => (
+              <View key={dish.id} className="all-food">
+                <View className="product-Picture">
+                  <ProductImage src={dish.avatar} mode="aspectFit" />
+                </View>
+                <View className="food-info">
+                  <View className="food-name">{dish.name}</View>
+                  <View className="food-describe">
+                    {dish.desc || "暂无描述"}
+                  </View>
+                  <View className="food-sales">销量:{dish.sales}</View>
+                  <View className="food-price">¥ {dish.price}</View>
+
+                  {showInputNumbers[dish.id] ? (
+                    <View className="food-inputNumber">
+                      <InputNumber
+                        defaultValue={1}
+                        min={1}
+                        value={inputValues[dish.id]}
+                        onChange={(value) => handleValueChange(value, dish.id)}
+                        onMinus={() => handleMinusClick(dish.id)}
+                      />
+                    </View>
+                  ) : (
+                    <View
+                      className={`food-input ${
+                        inputValues[dish.id] === 1 && !showInputNumbers[dish.id]
+                          ? "warning"
+                          : ""
+                      }`}
+                      onClick={() => toggleInputNumber(dish.id)}
+                    >
+                      <image src={plusIcon}></image>
+                    </View>
+                  )}
+                </View>
+              </View>
+            ))}
+          </View>
+        ))}
+      </View>
+
+      <View className="product-order">
+        <Badge
+          style={{
+            marginInlineEnd: "10px",
+            "--nutui-badge-height": "12px",
+            "--nutui-badge-padding": "0 3px",
+            "--nutui-badge-font-size": "10px",
+            "--nutui-badge-min-width": "3px",
+          }}
+          value={8}
+        >
+          <image src={shoppingCar} mode="aspectFit" />
+        </Badge>
+        <View className="product-dine">开炫</View>
       </View>
     </View>
   );
